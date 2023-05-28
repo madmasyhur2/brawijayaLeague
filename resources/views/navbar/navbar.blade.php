0d6c432c1d9a2d3fd34ab10487baf35ce7115dbf
hanifdrmawn <darmawanhanif.hd@gmail.com>
2023-05-28 21:59:10 +0700

    footer

--- resources/views/navbar/navbar.blade.php
+++ resources/views/navbar/navbar.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.main')
 
 <header class="navbar h-[10vh] bg-[#0A3E70] flex justify-between">
-    <img src="/public/assets/logo-header.svg" class="m-[10px_20px]">
+    <img src="../assets/logo-header.svg" class="m-[10px_20px]">
     <div class="navbar flex gap-[40px] m-[0_20px]">
         <a href="" class="flex justify-center items-center">HOME</a>
         <div class="navbar-dropdown">
             <div class="navbar-dropdown-text">
                 <p>GAMES</p>
-                <img src="/public/assets/Vector-V.svg">
+                <img src="../assets/Vector-V.svg">
             </div>
             <div class="dropdown-content">
-                <a href="#">SCHEDULES</a>
+                <a href="">SCHEDULES</a>
                 <a href="#">STANDINGS</a>
             </div>
         </div>
         <div class="navbar-dropdown">
             <div class="navbar-dropdown-text">
                 <p>PROFILE</p>
-                <img src="/public/assets/Vector-V.svg">
+                <img src="../assets/Vector-V.svg">
             </div>
             <div class="dropdown-content">
                 <a href="#">TEAMS</a>
@@ -28,7 +28,7 @@
         <a href="">VIDEOS</a>
         <a href="">NEWS</a>
         <a href="">ABOUT US</a>
-        <a href="/signUp/index.html" class="navbar-sign-up"><p>Sign Up</p></a>
+        {{-- <a href="/signUp/index.html" class="navbar-sign-up"><p>Sign Up</p></a> --}}
         <a href="/login/index.html" class="navbar-login"><p>Log In</p></a>
     </div>
 </header>
