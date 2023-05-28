a811395b70675e21c2f10a036b2f6dd6f6f295e8
hanifdrmawn <darmawanhanif.hd@gmail.com>
2023-05-28 21:38:26 +0700

    header

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Brawijaya League</title>
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+    <link rel="icon" href="/public/assets/Icon.png">
+    <link rel="stylesheet" href="resources/css/app.css">
+</head>
