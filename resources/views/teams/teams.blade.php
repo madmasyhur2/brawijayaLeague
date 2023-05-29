e0872d32f3e5c654cf2185bc21bac949a6a76569
hanifdrmawn <darmawanhanif.hd@gmail.com>
2023-05-29 11:45:06 +0700

    update

--- resources/views/teams/teams.blade.php
+++ resources/views/teams/teams.blade.php
@@ -2,7 +2,7 @@
 @include('navbar.navbar')
 
 <div class="main-tittle bg-[url(/public/assets/tittle-bg.svg)] h-[20vh] bg-no-repeat bg-cover">
-    <h1 class="pt-[30px] pl-[10%] text-[#FAFAFA] font-bold text-[60px] font-['Montserrat']">TEAMS</h1>
+    <h1 class="pt-[30px] pl-[10%] text-[#FAFAFA] font-bold text-5xl font-['Montserrat']">TEAMS</h1>
 </div>
 <div class="content m-20 ml-[10%] grid grid-cols-3">
     <div class="box border rounded-md border-[#E5E5E5] flex p-4 items-center m-1">
