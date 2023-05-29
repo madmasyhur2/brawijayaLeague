cb7d0a0ec5a5f348a604e1666d1ebe3b449f0ee7
hanifdrmawn <darmawanhanif.hd@gmail.com>
2023-05-29 08:01:16 +0700

    modified:   resources/views/standings/standings.blade.php

--- resources/views/standings/standings.blade.php
+++ resources/views/standings/standings.blade.php
@@ -21,6 +21,23 @@
             <th class="p-3 bg-[#006DCB] rounded-[0_6px_0_0]">GD</th>
         </tr>
 
+        {{-- @foreach ($standings as $standing)
+        <tr class="border border-[#E5E5E5]">
+            <td class="w-[6%] p-1 text-center font-bold text-l">{{ $standing->pos }}</td>
+            <td class="w-1/3 p-1 font-bold text-l flex gap-3 items-center">
+                <img class="m-0.5 h-9" src="{{ asset('storage/' . $standing->image) }}">
+                <p class="">{{ $standing->name }}</p>
+            </td>
+            <td class="w-[6%] p-1 text-center font-bold text-l">{{ $standing->pts }}</td>
+            <td class="w-[6%] p-1 text-center font-bold text-l">{{ $standing->g }}</td>
+            <td class="w-[6%] p-1 text-center font-bold text-l">{{ $standing->w }}</td>
+            <td class="w-[6%] p-1 text-center font-bold text-l">{{ $standing->d }}</td>
+            <td class="w-[6%] p-1 text-center font-bold text-l">{{ $standing->l }}</td>
+            <td class="w-[6%] p-1 text-center font-bold text-l">{{ $standing->gf }}</td>
+            <td class="w-[6%] p-1 text-center font-bold text-l">{{ $standing->ga }}</td>
+            <td class="w-[6%] p-1 text-center font-bold text-l">{{ $standing->gd }}</td>
+        </tr>
+        @endforeach --}}
 
         <tr class="border border-[#E5E5E5]">
             <td class="w-[6%] p-1 text-center font-bold text-l"></td>
