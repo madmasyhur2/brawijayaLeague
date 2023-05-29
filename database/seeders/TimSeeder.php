b84c2729201dd7f69d2b2d05194dc49136a27ce1
madmasyhur <muhammadmasyhur2@gmail.com>
2023-05-29 09:02:05 +0700

    coba data seeder

--- database/seeders/TimSeeder.php
+++ database/seeders/TimSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Tim;
 
 class TimSeeder extends Seeder
 {
@@ -12,6 +13,28 @@ class TimSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Tim::create([
+            "logo_tim" => "logo.jpg",
+            "nama_tim" => "chelsea",
+            "pos" => 10,
+            "poin" => 40,
+            "menang"=> 10,
+            "seri" => 10,
+            "kalah" => 10,
+            "gol" => 30,
+            "kebobolan" => 10,
+        ]);
+
+        Tim::create([
+            "logo_tim" => "logo1.jpg",
+            "nama_tim" => "Manchester United",
+            "pos" => 1,
+            "poin" => 90,
+            "menang"=> 30,
+            "seri" => 0,
+            "kalah" => 8,
+            "gol" => 90,
+            "kebobolan" => 20,
+        ]);
     }
 }
