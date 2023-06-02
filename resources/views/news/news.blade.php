c81d75bad60bd93e6e160bba700a46aed20ae05b
kesiddewa <kesiddewa600@gmail.com>
2023-06-02 22:27:20 +0700

    final news

--- resources/views/news/news.blade.php
+++ resources/views/news/news.blade.php
@@ -9,16 +9,16 @@
 
     @foreach ($posts as $p)
     <div class="line2-column1 bg-[#FFFFFF] border border-solid border-[#E5E5E5] rounded-md h-[270px] relative">
-        <div class="image relative overflow-hidden rounded-[6px_6px_0_0]">
+        <div class="image overflow-hidden rounded-[6px_6px_0_0]">
             <img src="{{ '../assets/news/'.$p->gambar_berita }}" class="object-fill w-full h-fit transform-gpu transition-transform duration-300 ease-out hover:scale-125" alt="gambar-news">
-        </div>
-        <h2 class="font-bold text-[20px] text-[#0A0A0A] m-4 mt-2 font-['Roboto']">{{ Illuminate\Support\Str::limit($p->judul, 55) }}</h2>
-        <p class="absolute bottom-3 left-4 font-['Roboto'] text-[#737373] text-[14px] font-medium">{{ $p->tgl_publikasi }}</p>
+            <div class="line2-column1 bg-[#FFFFFF] rounded-b-lg h-[110px] absolute bottom-0">
+                <h2 class="font-bold text-[20px] text-[#0A0A0A] m-4 mt-2 font-['Roboto']">{{ Illuminate\Support\Str::limit($p->judul, 55) }}</h2>
+                <p class="absolute bottom-3 left-4 font-['Roboto'] text-[#737373] text-[14px] font-medium">{{ $p->tgl_publikasi }}</p>
+            </div>
+        </div >
     </div>
     @endforeach
-
-
-
+    
 </div>
 
 
