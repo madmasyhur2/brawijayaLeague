cf18e11cc1509b1a6562517bc8ef43bc740424a8
madmasyhur <muhammadmasyhur2@gmail.com>
2023-06-06 09:50:23 +0700

    update gambar

--- resources/views/schedules/schedules.blade.php
+++ resources/views/schedules/schedules.blade.php
@@ -13,36 +13,16 @@
         <div class="boardvalue-tim s32">
             <div class="tim s20">
                 <p>{{ $ps->home_name }}</p>
-                <img class="m-2" src="{{'../assets/landingpage/logotim.svg'.$ps->home_logo}}" alt="">
+                <img class="m-2" src="{{'../storage/teamLogo/'.$ps->home_logo}}" alt="">
             </div>
             <p>VS</p>
             <div class="tim2 s20">
-                <img class="m-2" src="{{'../assets/landingpage/logotim.svg'.$ps->away_logo}}" alt="">
+                <img class="m-2" src="{{'../storage/teamLogo/'.$ps->away_logo}}" alt="">
                 <p>{{ $ps->away_name }}</p>
             </div>
         </div>
     </div>
     @endforeach
-
-    {{-- @foreach ($pertandingans as $pertandingan)
-    <div class="boardvalue">
-        <div class="boardvalue-date s14">
-            <p>{{ $pertandingan->day }}</p>
-            <p>{{ $pertandingan->time }}</p>
-        </div>
-        <div class="boardvalue-tim s32">
-            <div class="tim s20">
-                <p>{{ $pertandingan->tim1 }}</p>
-                <img class="m-2" src="{{ asset('storage/' . $pertandingan->imagetim1) }}" alt="">
-            </div>
-            <p>VS</p>
-            <div class="tim2 s20">
-                <img class="m-2" src="{{ asset('storage/' . $pertandingan->imagetim2) }}" alt="">
-                <p>{{ $pertandingan->tim2 }}</p>
-            </div>
-        </div>
-    </div>
-    @endforeach --}}
 </div>
 
 
