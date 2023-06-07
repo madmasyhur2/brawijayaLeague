59af99248cd6f26f244e33907b367b5dc05b8496
hanifdrmawn <darmawanhanif.hd@gmail.com>
2023-06-07 09:54:24 +0700

    modified:   resources/css/app.css 	modified:   resources/views/admin/admin.blade.php 	modified:   resources/views/admin/sidebar/sidebar.blade.php

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -2,7 +2,20 @@
 
 <div class="flex">
     @include('admin.sidebar.sidebar')
-    <div class="m-8 ml-44">
-
+    <div id="content" class="m-8 ml-[20%] mt-56 text-7xl font-bold text-Text-color text-center">
     </div>
 </div>
+<script>
+    const textElement = document.getElementById('content');
+    const text = "Welcome to Page Admin";
+    let index = 0;
+
+    function type() {
+        textElement.textContent += text.charAt(index);
+        index++;
+        if (index < text.length) {
+            setTimeout(type, 100);
+        }
+    }
+    type();
+</script>
