237cdf43a34090cfab615fc3ecf16ac09a013739
hanifdrmawn <darmawanhanif.hd@gmail.com>
2023-06-07 11:52:04 +0700

    modified:   resources/views/about/about.blade.php

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.main')
 @include('navbar.navbar')
-<div class=" m-20 grid grid-cols-4 gap-10">
-    <div class="border rounded-lg">
+<div class=" m-20 grid grid-cols-4 gap-14">
+    <div class="border rounded-lg hover:scale-125 transition duration-300 ease-in-out">
         <div class=" w-56 h-56 mx-auto my-2">
             <img class="w-full h-full bg-primary rounded-full" src="https://i.ibb.co/HDJjszQ/mamad.png">
         </div>
         <p class="text-center font-bold bg-Text-color text-Secondary-1 p-1 text-2xl rounded-[0_0_8px_8px]">M. Bin Dja'far A.</p>
     </div>
-    <div class="border rounded-lg">
+    <div class="border rounded-lg hover:scale-125 transition duration-300 ease-in-out">
         <div class=" w-56 h-56 mx-auto my-2">
             <img class="w-full h-full bg-primary rounded-full" src="https://i.ibb.co/tYgdWLr/hanif.png">
         </div>
         <p class="text-center font-bold bg-Text-color text-Secondary-1 p-1 text-2xl rounded-[0_0_8px_8px]">Hanif Darmawan</p>
     </div>
-    <div class="border rounded-lg">
+    <div class="border rounded-lg hover:scale-125 transition duration-300 ease-in-out">
         <div class=" w-56 h-56 mx-auto my-2">
             <img class="w-full h-full bg-primary rounded-full" src="https://i.ibb.co/d0VWSzs/ariz.png">
         </div>
         <p class="text-center font-bold bg-Text-color text-Secondary-1 p-1 text-2xl rounded-[0_0_8px_8px]">M. Rizqon Maulana</p>
     </div>
-    <div class="border rounded-lg">
+    <div class="border rounded-lg hover:scale-125 transition duration-300 ease-in-out">
         <div class=" w-56 h-56 mx-auto my-2">
             <img class="w-full h-full bg-primary rounded-full" src="https://i.ibb.co/LNtCmn3/Whats-App-Image-2023-06-07-at-11-37-31-removebg-preview.png">
         </div>
