d3c141878cad2e282cd525c8aa2c81a5694b802c
hanifdrmawn <darmawanhanif.hd@gmail.com>
2023-06-07 11:55:31 +0700

    modified:   resources/views/about/about.blade.php

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.main')
 @include('navbar.navbar')
-<div class=" m-20 grid grid-cols-4 gap-14">
-    <div class="border rounded-lg hover:scale-125 transition duration-300 ease-in-out">
+<div class="mx-20 my-32 grid grid-cols-4 gap-14">
+    <div class="hover:scale-125 transition duration-300 ease-in-out">
         <div class=" w-56 h-56 mx-auto my-2">
             <img class="w-full h-full bg-primary rounded-full" src="https://i.ibb.co/HDJjszQ/mamad.png">
         </div>
-        <p class="text-center font-bold bg-Text-color text-Secondary-1 p-1 text-2xl rounded-[0_0_8px_8px]">M. Bin Dja'far A.</p>
+        <p class="text-center font-bold bg-Text-color text-Secondary-1 p-1 text-2xl rounded-lg">M. Bin Dja'far A.</p>
     </div>
-    <div class="border rounded-lg hover:scale-125 transition duration-300 ease-in-out">
+    <div class="hover:scale-125 transition duration-300 ease-in-out">
         <div class=" w-56 h-56 mx-auto my-2">
             <img class="w-full h-full bg-primary rounded-full" src="https://i.ibb.co/tYgdWLr/hanif.png">
         </div>
-        <p class="text-center font-bold bg-Text-color text-Secondary-1 p-1 text-2xl rounded-[0_0_8px_8px]">Hanif Darmawan</p>
+        <p class="text-center font-bold bg-Text-color text-Secondary-1 p-1 text-2xl rounded-lg">Hanif Darmawan</p>
     </div>
-    <div class="border rounded-lg hover:scale-125 transition duration-300 ease-in-out">
+    <div class="hover:scale-125 transition duration-300 ease-in-out">
         <div class=" w-56 h-56 mx-auto my-2">
             <img class="w-full h-full bg-primary rounded-full" src="https://i.ibb.co/d0VWSzs/ariz.png">
         </div>
-        <p class="text-center font-bold bg-Text-color text-Secondary-1 p-1 text-2xl rounded-[0_0_8px_8px]">M. Rizqon Maulana</p>
+        <p class="text-center font-bold bg-Text-color text-Secondary-1 p-1 text-2xl rounded-lg">M. Rizqon Maulana</p>
     </div>
-    <div class="border rounded-lg hover:scale-125 transition duration-300 ease-in-out">
+    <div class="hover:scale-125 transition duration-300 ease-in-out">
         <div class=" w-56 h-56 mx-auto my-2">
             <img class="w-full h-full bg-primary rounded-full" src="https://i.ibb.co/LNtCmn3/Whats-App-Image-2023-06-07-at-11-37-31-removebg-preview.png">
         </div>
-        <p class="text-center font-bold bg-Text-color text-Secondary-1 p-1 text-2xl rounded-[0_0_8px_8px]">Kesid Dewa W.</p>
+        <p class="text-center font-bold bg-Text-color text-Secondary-1 p-1 text-2xl rounded-lg">Kesid Dewa W.</p>
     </div>
 </div>
 @include('footer.footer')
